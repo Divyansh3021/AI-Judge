669c52cffc3bb1e04da452e041fa9a78670e2288
Divyansh3021 <divyansh3021@gmail.com>
2023-12-25 20:48:50 +0530

    updated requirements.txt

--- AztecPushupModule.py
+++ AztecPushupModule.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import utils
 
+
 class AztecPushupCounter:
     def __init__(self):
         self.pose = mp.solutions.pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
