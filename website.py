6f18df952c3cb06fdf7cfdccfc98dc7a305baff7
Divyansh3021 <divyansh3021@gmail.com>
2023-12-26 15:08:46 +0530

    website updated

--- website.py
+++ website.py
@@ -1,14 +1,22 @@
 from streamlit_webrtc import VideoHTMLAttributes, webrtc_streamer
 from aiortc.contrib.media import MediaRecorder
 import streamlit as st
-# from pushupModule import PushupCounter
-# from PullupModule import PullupCounter
+from pushupModule import PushupCounter
+from PullupModule import PullupCounter
 from AztecPushupModule import AztecPushupCounter
 import av
 
 st.title("Sports AI Judge")
 
-live_frame_process = AztecPushupCounter()
+exercises = ["Pushup", "Pullup", "Aztec Pushup"]
+selected_exercise = st.selectbox("Select Exercise", exercises)
+
+if selected_exercise == "Pushup":
+    live_frame_process = PushupCounter()
+elif selected_exercise == "Pullup":
+    live_frame_process = PullupCounter()
+elif selected_exercise == "Aztec Pushup":
+    live_frame_process = AztecPushupCounter()
 
 def video_frame_callback(frame: av.VideoFrame):
     frame = frame.to_ndarray(format="rgb24")
@@ -25,7 +33,7 @@ ctx = webrtc_streamer(
                         key="Squats-pose-analysis",
                         video_frame_callback=video_frame_callback,
                         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},  # Add this config
-                        media_stream_constraints={"video": {"width": {'min':480, 'ideal':480}}, "audio": False},
-                        video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False),
+                        media_stream_constraints={"video": {"width": 1920, "height":1080}, "audio": False},
+                        video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False, width="100%", height = "100%"),
                         out_recorder_factory=out_recorder_factory
                     )
